a9c1760de01e577a8f6b97c6848fa0ed498835a8
Jardani <rtran@Jardanis-MacBook-Pro.local>
2024-03-15 05:19:29 -0700

    First commit

---
+++ todo_app/target/debug/deps/todo_app-c84af26100e82df7.d
@@ -0,0 +1,5 @@
+/Users/rtran/Documents/Et al Rust/Et al Rust/todo_app/target/debug/deps/todo_app-c84af26100e82df7: src/main.rs
+
+/Users/rtran/Documents/Et al Rust/Et al Rust/todo_app/target/debug/deps/todo_app-c84af26100e82df7.d: src/main.rs
+
+src/main.rs:
---
+++ todo_app/target/debug/todo_app.d
@@ -0,0 +1,1 @@
+/Users/rtran/Documents/Et\ al\ Rust/Et\ al\ Rust/todo_app/target/debug/todo_app: /Users/rtran/Documents/Et\ al\ Rust/Et\ al\ Rust/todo_app/src/main.rs
